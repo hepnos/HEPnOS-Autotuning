5be1f78ec64641a71d71893d43e0da4843e840f7
Matthieu Dorier <mdorier@anl.gov>
2020-08-14 08:50:38 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+HEPnOS Autotuning
+=================
+
+Installing
+----------
+
+```
+./install.sh
+```
+
+
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+function log {
+    printf "\u2550\u2550> $1\n"
+}
+
+WD=`pwd`
+log "Cloning spack..."
+git clone https://github.com/spack/spack.git
+
+log "Setting up spack..."
+. ./spack/share/spack/setup-env.sh
+
+log "Finding compilers..."
+spack compiler find
+
+log "Cloning mochi namespace..."
+git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
+
+log "Creating hepnos environment..."
+MOCHI_REPO=$WD/sds-repo
+cat env/spack.yaml.in \
+    | sed -e "s|\${MOCHI_REPO}|${MOCHI_REPO}|g" \
+    > env/spack.yaml
+spack env create hepnos env/spack.yaml
+
+log "Activating hepnos environment..."
+spack env activate hepnos
+
+log "Installing HEPnOS and DataLoader..."
+spack install
