0929c33d27cba26440025efea3b3238361cf5238
Matthieu Dorier <mdorier@anl.gov>
2021-03-15 10:46:08 +0000

    added possibility to change PES per node and number of cores per PE

--- theta/run.py
+++ theta/run.py
@@ -11,7 +11,8 @@ def __setup_directory():
 
 
 def __create_settings(exp_dir, loader_batch_size, loader_progress_thread,
-                      pep_num_threads, pep_ibatch_size, pep_obatch_size):
+                      pep_num_threads, pep_ibatch_size, pep_obatch_size,
+                      pep_pes_per_node, pep_cores_per_pe):
     settings_sh_in = os.path.dirname(os.path.abspath(__file__)) + '/scripts/settings.sh.in'
     settings_sh = exp_dir + '/settings.sh'
     copyfile(settings_sh_in, settings_sh)
@@ -27,6 +28,8 @@ def __create_settings(exp_dir, loader_batch_size, loader_progress_thread,
             f.write('HEPNOS_PEP_THREADS=%d\n' % pep_num_threads)
             f.write('HEPNOS_PEP_IBATCH_SIZE=%d\n' % pep_ibatch_size)
             f.write('HEPNOS_PEP_OBATCH_SIZE=%d\n' % pep_obatch_size)
+            f.write('HEPNOS_PEP_PES_PER_NODE=%d\n' % pep_pes_per_node)
+            f.write('HEPNOS_PEP_CORES_PER_PE=%d\n' % pep_cores_per_pe)
         else:
             f.write('HEPNOS_ENABLE_PEP=0\n')
 
@@ -80,9 +83,11 @@ def __parse_result(exp_dir):
 
 def run(args):
     if len(args) == 5:
-        args.extend([None, None, None])
-    if len(args) != 8:
-        raise RuntimeError("Expected 5 or 8 arguments in list, found %d" % len(args))
+        args.extend([None, None, None, None, None])
+    if len(args) == 8:
+        args.extend([2, 32])
+    if len(args) != 10:
+        raise RuntimeError("Expected 5 or 10 arguments in list, found %d" % len(args))
     hepnos_num_threads = args[0]
     hepnos_num_databases = args[1]
     busy_spin = args[2]
@@ -91,6 +96,8 @@ def run(args):
     pep_num_threads = args[5]
     pep_ibatch_size = args[6]
     pep_obatch_size = args[7]
+    pep_pes_per_node = args[8]
+    pep_cores_per_pe = args[9]
     print('Setting up experiment\'s directory')
     exp_dir = __setup_directory()
     print('Creating settings.sh')
@@ -99,7 +106,9 @@ def run(args):
                       loader_progress_thread,
                       pep_num_threads,
                       pep_ibatch_size,
-                      pep_obatch_size)
+                      pep_obatch_size,
+                      pep_pes_per_node,
+                      pep_cores_per_pe)
     print('Creating config.yaml')
     __generate_config_file(
             exp_dir,
@@ -123,8 +132,16 @@ if __name__ == '__main__':
     # - the batch size for the dataloader
     # If you want to run the PEP benchmark as well,
     # the following parameters must be added:
-    # - number of threads per benchmark process (must be > 0)
+    # - number of processing threads per benchmark process (must be > 0)
     # - batch size when loading from HEPnOS
     # - batch size when loading from another rank
-    run([ 31, 1, False, False, 1024, 31, 32, 1024 ])
+    # If you want to configure the number of PES and cores per PE for
+    # the benchmark, you can add two more parameters:
+    # - number of PES per node (must be between 1 and 64)
+    # - number of cores per PE (must be between 1 and 64)
+    # The product of the these two parameters should not exceed 64.
+    # Additionally, the number of processing threads should be
+    # the number of cores per PE minus 2 (so effectively the number
+    # cores per PE must be at least 3).
+    run([ 31, 1, False, False, 1024, 31, 32, 1024, 2, 32 ])
     #run([ 31, 1, False, False, 1024 ])
