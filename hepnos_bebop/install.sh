47394990dbd215a0b53514516e11b1c9a6a1cfbf
Matthieu Dorier <mdorier@anl.gov>
2021-08-25 15:24:25 -0500

    added installation of deephyper

--- hepnos_bebop/install.sh
+++ hepnos_bebop/install.sh
@@ -30,3 +30,32 @@ spack env activate hepnos
 
 log "Installing HEPnOS and DataLoader..."
 spack install
+
+# Create Conda Env
+log "Creating conda environment..."
+conda create -p dhenv python=3.8 -y
+conda activate dhenv/
+conda install gxx_linux-64 gcc_linux-64 -y
+
+# Install ConfigSpace with Truncated Normal Distribution
+log "Cloning ConfigSpace repo and installing..."
+git clone https://github.com/deephyper/ConfigSpace.git
+cd ConfigSpace/
+git checkout truncated_normal
+pip install -e.
+cd ..
+
+# Installing Scikit-Optimize with ConfigSpace
+log "Cloning Scikit-Optimize repo and installing..."
+git clone https://github.com/deephyper/scikit-optimize.git
+cd scikit-optimize/
+pip install -e.
+cd ..
+
+# Install DeepHyper
+log "Cloning DeepHyper repo and installing..."
+git clone https://github.com/pbalapra/deephyper.git
+cd deephyper/
+git checkout develop
+pip install -e.
+cd ..
