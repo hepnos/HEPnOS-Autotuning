f384a4043ca9ecb47c2a25a51ddddc3575af88ef
Matthieu Dorier <mdorier@anl.gov>
2021-08-30 07:22:44 -0500

    separated installation of HEPnOS and DeepHyper

---
+++ hepnos_bebop/install_deephyper.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+HERE=`dirname $0`
+HERE=`realpath $HERE`
+
+function log {
+    printf "\u2550\u2550> $1\n"
+}
+
+module load gcc/8.2.0-g7hppkz
+
+# Setting up Spack
+log "Setting up Spack"
+. $HERE/spack/share/spack/setup-env.sh
+
+# Activating HEPnOS environment
+log "Activating HEPnOS environment"
+spack env activate hepnos
+
+# Create Conda Env
+log "Loading modules and creating conda environment..."
+conda create -p dhenv python=3.8 -y
+conda activate dhenv/
+conda install gxx_linux-64 gcc_linux-64 -y
+
+# Install ConfigSpace with Truncated Normal Distribution
+log "Cloning ConfigSpace repo and installing..."
+git clone https://github.com/deephyper/ConfigSpace.git
+cd ConfigSpace/
+git checkout truncated_normal
+pip install -e.
+cd ..
+
+# Installing Scikit-Optimize with ConfigSpace
+log "Cloning Scikit-Optimize repo and installing..."
+git clone https://github.com/deephyper/scikit-optimize.git
+cd scikit-optimize/
+pip install -e.
+cd ..
+
+# Install DeepHyper
+log "Cloning DeepHyper repo and installing..."
+git clone https://github.com/pbalapra/deephyper.git
+cd deephyper/
+git checkout develop
+pip install -e.
+cd ..
+
