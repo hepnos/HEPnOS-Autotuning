a90fa5f23ab9ccce9da5f3d4f016944346a77663
Matthieu Dorier <mdorier@anl.gov>
2022-02-08 07:55:07 -0600

    huge cleanup

--- hepnos/autotuning/bebop/test/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf databases.json hepnos.ssg dataloader-output.txt pep-output.txt slurm-* core*
-
--- hepnos_theta/scripts/old_scripts/working_ray_not_setup/job_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activate env or conda env if applicable:
-source activate /lus/theta-fs0/projects/OptADDN/hepnos/sw/dh-env-2
-cd exp/
-mkdir exp_run
-cd exp_run
-deephyper hps ambs --evaluator ray --problem hps_1.hepnos.problem.Problem --run hps_1.hepnos.model_run.run --num-workers 32 --n-jobs 32
